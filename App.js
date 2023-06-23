21063cc0882a8fb4d83bf9cfb6f68787d6b7d75c
Nikhil Sai Jorrigala <111264338+nikki312@users.noreply.github.com>
2023-06-23 23:59:54 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MainScreen from './MainScreen';
+import NewScreen from './NewScreen';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="MainScreen"
+          component={MainScreen}
+          options={{
+            headerTitle: () => (
+              <View>
+                <Image source={require('./assets/ss.png')} style={styles.headerImage} />
+              </View>
+            ),
+          }}/>   
+        <Stack.Screen name="NewScreen" component={NewScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  headerImage: {
+    width: 400,
+    height: 50,
+    resizeMode: 'contain',
   },
 });
+
+
+export default App;
+      
