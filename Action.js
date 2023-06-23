6ef36ad7bd202206b6cb62c0704a65e79f82c719
Nikhil Sai Jorrigala <111264338+nikki312@users.noreply.github.com>
2023-06-24 00:00:56 +0530

    Add files via upload

---
+++ Action.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const Action = ({ selectedSubparts, handleRemoveSubpart }) => {
+  const [inputValue, setInputValue] = useState('');
+  const navigation = useNavigation();
+
+  const handleAddSubpart = () => {
+    if (inputValue) {
+      handleRemoveSubpart([...selectedSubparts, inputValue]);
+      setInputValue('');
+    }
+  };
+
+  const handleSaveAllInputs = () => {
+    
+    storeSelectedSubparts(selectedSubparts);
+    navigation.navigate('MainScreen'); // Navigate to the main screen
+  };
+
+  const storeSelectedSubparts = (subparts) => {
+    console.log('Stored Selected Subparts:', subparts);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Action</Text>
+      <View style={styles.selectedSubpartsContainer}>
+        {selectedSubparts.map((subpart) => (
+          <View key={subpart} style={styles.selectedSubpart}>
+            <Text style={styles.selectedSubpartText}>{subpart}</Text>
+            <TouchableOpacity onPress={() => handleRemoveSubpart(subpart)}>
+            </TouchableOpacity>
+          </View>
+        ))}
+      </View>
+      <TouchableOpacity onPress={handleSaveAllInputs} style={styles.saveAllButton}>
+        <Text style={styles.saveAllButtonText}>Save All</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#f0f0f0',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  selectedSubpartsContainer: {
+    marginTop: 20,
+  },
+  selectedSubpart: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 5,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 5,
+  },
+  selectedSubpartText: {
+    flex: 1,
+    fontSize: 14,
+  },
+  saveAllButton: {
+    marginTop: 20,
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  saveAllButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
+
+export default Action;
